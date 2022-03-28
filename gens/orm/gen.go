ab24b9c5beda5b2a6a1298244d2761e386f49d4d
cinos1 <cinos@qq.com>
2022-03-28 14:21:24 +0800

    增加事件类型格式化公共model

--- gens/orm/gen.go
+++ gens/orm/gen.go
@@ -78,6 +78,7 @@ func Gen(dbFile string, name string, tbs string) {
 		db := v.(map[interface{}]interface{})
 		//生成表service代码
 		loadDBMetaInfo(tbs, db)
+		genutils.GenFileWithTargetPath("model/localTime.go.tmpl", "model/localTime.go", db)
 		sh.Command("gofmt", "-w", ".", sh.Dir("model")).Run()
 		//生成api层表相关代码
 		for _, tableMeta := range db["TableMetas"].([]interface{}) {
